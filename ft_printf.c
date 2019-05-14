d9bf50b1aa159fc0fc4bd69842b10ebc08f98237
Sandra <sgury@student.42.fr>
2019-05-14 17:26:56 +0200

    ft_parse fonctionnel + ft_printf + .h modifie

--- ft_printf.c
+++ ft_printf.c
@@ -6,19 +6,30 @@
 /*   By: sgury <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/13 19:16:08 by sgury             #+#    #+#             */
-/*   Updated: 2019/05/13 19:43:47 by sgury            ###   ########.fr       */
+/*   Updated: 2019/05/14 17:24:50 by sgury            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdarg.h>
+#include "ft_printf.h"
 
 void	ft_printf(const char *format, ...)
 {
 	t_data_tab	*data;
+	int			index;
+	va_list		ap;
 
+	va_start(ap, format);
 	if ((data = (t_data_tab *)malloc(sizeof(t_data_tab))) == NULL)
 		return ;
-	while((ret = ft_parse(format + ret, data)) > 0)
+	index = 0;
+	while((index = ft_parse(format, data, index)) > 0)
 	{
-		if (ft_dispatcher(ap, data) < 0);
-		return ;
+		if (ft_dispatcher(ap, data) < 0)
+			return ;
+		ft_bzero(data->flags, FLAG_MAX);
+		data->conv = '\0';
 	}
+	va_end(ap);
+	ft_free_data(data);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,25 +6,28 @@
 /*   By: sgury <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/13 09:28:29 by sgury             #+#    #+#             */
-/*   Updated: 2019/05/13 19:43:44 by sgury            ###   ########.fr       */
+/*   Updated: 2019/05/14 17:25:22 by sgury            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
-# define FT_PRINT_H
+# define FT_PRINTF_H
 
-#define FLAG_MAX
-#define CONVERSION "sdiouxXfcp"
+# include "./libft/libft.h"
+
+# define FLAG_MAX 6
+# define NB_CONV 10
+# define CONVERSION "sdiouxXfcp"
 
 typedef struct	s_data_tab
 {
 	char flags[FLAG_MAX];
 	char conv;
-}				t_opt_tab
+}				t_data_tab;
 
 void			ft_printf(const char *format, ...);
-int				ft_parse(char *str, t_data_tab *data);
-int				ft_dispacher(va_list ap, t_data_tab *data);
+int				ft_parse(char *str, t_data_tab *data, int index);
+//int				ft_dispacher(va_list ap, t_data_tab *data);
 
 
 #endif
