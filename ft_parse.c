565d5484d41cb8a2deceba2cb1f79b557c60e694
Sandra <sgury@student.42.fr>
2019-05-17 11:19:16 +0200

    parseur need to be tested

--- ft_parse.c
+++ ft_parse.c
@@ -6,57 +6,85 @@
 /*   By: sgury <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/14 11:28:33 by sgury             #+#    #+#             */
-/*   Updated: 2019/05/14 17:17:31 by sgury            ###   ########.fr       */
+/*   Updated: 2019/05/17 11:18:33 by sgury            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int		ft_get_data(char *str, t_data_tab *data)
+int		ft_get_nbr(char *str, int index)
+{
+	int	nbr;
+	int	precision;
+
+	nbr = 0;
+	precision = 0;
+	if (str[index++] == '.')
+		precision = 1;
+	while (ft_isdigit(str[index]))
+		nbr = nbr * 10 + str[index++];
+	if (precision)
+		data.precision = nbr;
+	else
+		data.width = nbr;
+	return (index);
+}
+
+int		ft_get_flag(char *str, t_data_tab data, int index)
+{
+	static int	*flags_address[FLAGS_NB] = {&data.just_left, &data.sign,
+		&data.space, &data.zero, &data.hashtag, &data.h, &data.l, &data.L};
+	static char	*flags_char[FLAGS_NB] = {'-', '+', ' ',
+		'0', '#', 'h', 'l', 'L'};
+	int			i;
+	int			precision;
+
+	while (str[index] != flags_char[i] && i < FLAGS_NB)
+		i++;
+	if ((i == 5 || i == 6) && str[index] == str[index + 1])
+	{
+		if (str[index] == 'h')
+			data.hh = 1;
+		if (str[index] == 'l')
+			data.ll = 1;
+		return (index + 2);
+	}
+	if (str[index] != flags_char[i])
+		return (ft_get_nbr(str, index));
+	*flags_address[i] = 1;
+	return (++index);
+}
+
+int		ft_get_data(char *str, t_data_tab data, int index)
 {
 	char	conv[NB_CONV] = "sdiouxXfcp";
 	int		i;
-	int		j;
-	int		ret;
 
 	i = 0;
-	j = 0;
-	ret = 1;
-	str++;
-	if (*str == '%')
+	if (str[++index] == '%')
 	{
 		ft_putchar('%');
-		return (2);
+		return (++index);
 	}
-	while (*str != conv[i] && ret <= FLAG_MAX)
+	while (str[index] != conv[i])
 	{
-		while (i < NB_CONV && *str != conv[i])
+		while (i < NB_CONV && str[index] != conv[i])
 			i++;
-		if (i == NB_CONV && *str != conv[i])
-		{
-			data->flags[j++] = *str;
-			i = 0;
-			str++;
-		}
-		ret++;
+		if (i == NB_CONV && str[index] != conv[i])
+			index = ft_get_flag(str, data, index);
+		i = 0;
 	}
-	if (ret > FLAG_MAX)
-		return (-1);
-	data->conv = *str;
-	return (ret);
+	data.conv = str[index++];
 }
 
-int		ft_parse(char *str, t_data_tab *data, int i)
+int		ft_parse(char *str, t_data_tab data, int index)
 {
-	int	ret;
-
-	ret = 0;
-	while (str[i] != '\0' && str[i] != '%')
-		ft_putchar(str[i++]);
-	if (str[i] == '%')
+	while (str[index] != '\0' && str[index] != '%')
+		ft_putchar(str[index++]);
+	if (str[index] == '%')
 	{
-		if ((ret = ft_get_data(str + i, data)))
-			return (ret + i);
+		if ((index = ft_get_data(str, data, index)))
+			return (index);
 	}
 	return (0);
 }
