7c2218e50d0452c9e0cf4544069f9a4b69cd146a
Florian <flbeaumo@student.42.fr>
2019-05-22 15:57:26 +0200

    convert base to decimal work 👾🦄

--- ft_convert_base.c
+++ ft_convert_base.c
@@ -6,47 +6,68 @@
 /*   By: flbeaumo <flbeaumo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 10:32:14 by flbeaumo          #+#    #+#             */
-/*   Updated: 2019/05/20 11:58:09 by flbeaumo         ###   ########.fr       */
+/*   Updated: 2019/05/22 14:34:11 by flbeaumo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
 
-int     ft_is_spaces(char c)
+#define RED     "\x1b[31m"
+#define GREEN   "\x1b[32m"
+#define YELLOW  "\x1b[33m"
+#define BLUE    "\x1b[34m"
+#define MAGENTA "\x1b[35m"
+#define CYAN    "\x1b[36m"
+#define RESET   "\x1b[0m"
+
+static int      is_spaces(char c)
 {
     return ((c >= 9 && c <= 13) || c == 32);
 }
 
-int     ft_convert_base(char *str)
+static int      val_check(char c)
 {
-    int     result;
-    int     neg;
-
-    result = 0;
-    neg = 1;
-    while (ft_is_spaces(*str))
-        ++str;
-    if (*str == '-')
-        neg = -1;
-    if (*str == '+' || *str == '-')
-        ++str;
-    while (ft_isdigit(*str))
-    {
-        result *= 10;
-        result += *str - 48;
-        ++str;
-    }
-    return (result * neg);
+    if (c >= '0' && c <= '9')
+        return ((int)c - '0');
+    else
+        return ((int)c - 'A' + 10);
 }
+
+int     ft_convert_base(char *str, int base)
+{
+    int len;
+    int result;
+    int power;
+    int i;
+
+    len = ft_strlen(str);
+    result = 0;
+    power = 1;
+    i = len;
+    while (--i >= 0)
+    {
+        if (val_check(str[i]) >= base)
+            return (-1);    // Base or Number are invalid
+        printf(YELLOW"STR = %c --> "MAGENTA" true val: %d\n"RESET, str[i], val_check(str[i]));
+        printf(BLUE"Power = %d\n"RESET, power);
+        result += val_check(str[i]) * power;
+        power *= base;
+    }
+    return (result);
+}
+
+
+#include <stdlib.h>
 
 int     main(int ac, char **av)
 {
-    if (ac != 2)
+    if (ac != 3)
     {
         printf("[ERROR]: USAGE -> [./a.out] [NUMBER]");
         return (0);
     }
-    printf("%d\n", ft_convert_base(av[1]));
+    printf(GREEN"\nRESULT IN BASE 10: "RED" --> %d <--\n"RESET, ft_convert_base(av[1], atoi(av[2])));
+    /*printf("Value: %c\nBase: %d\nResult in base: 10 -> %d\n", 'F', 16, val_check('F'));*/
     return (0);
 }
--- ft_itoa_base.c
+++ ft_itoa_base.c
@@ -6,17 +6,18 @@
 /*   By: flbeaumo <flbeaumo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/17 14:44:35 by flbeaumo          #+#    #+#             */
-/*   Updated: 2019/05/20 13:12:15 by flbeaumo         ###   ########.fr       */
+/*   Updated: 2019/05/20 17:48:46 by flbeaumo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
-#include "libft/libft.h"
+#include "../ft_printf.h"
+#include "../libft/libft.h"
 #include "stdlib.h"
+#include <stdio.h>
 
 static int  ft_count(int nb)
 {
-    int count;
+    int  count;
 
     count = 0;
     while (nb)
@@ -27,14 +28,17 @@ static int  ft_count(int nb)
     return (count);
 }
 
-static char *reverse(char *str)
+static char     *reverse(char *str, int neg)
 {
     char    tmp;
     int     i;
     int     j;
 
     i = ft_strlen(str);
-    j = 0;
+    if (neg)
+        j = 1;
+    else
+        j = 0;
     --i;
     while (i > j)
     {
@@ -47,43 +51,71 @@ static char *reverse(char *str)
     return (str);
 }
 
-static char *free_str(char *str)
+static char     *free_str(char *str, int neg)
 {
     char    *tmp;
 
     tmp = str;
-    if ((tmp = reverse(str)) == NULL)
+    if ((tmp = reverse(str, neg)) == NULL)
         return (NULL);
     free(str);
     str = tmp;
     return (str);
 }
 
-char        *ft_itoa_base(long int nb, int base)
+static void     replace(int nb, char *str, int i, int neg)
 {
     static char     hexa[6] = "ABCDEF";
     static char     replace[6] = ":;<=>?";
-    char            *str;
-    int             i;
     int             j;
 
+    j = -1;
+    while (str[i] >= 57 && ++j < 6)
+    {
+        if (str[i] == replace[j])
+        {
+            str[i] = hexa[j];
+            break ;
+        }
+    }
+}
+
+char            *ft_itoa_base(long int nb, int base)
+{
+    char            *str;
+    int             i;
+    int             neg;
+
     i = 0;
+    neg = 0;
     if ((str = (char *)malloc(sizeof(char) * ft_count(nb))) == NULL)
         return (NULL);
-    while (nb)
+    if (nb < 0)
     {
-        j = -1;
-        str[i] = nb % base + '0';
-        nb /= base;
-        while (str[i] >= '9' && ++j < 6)
-        {
-            if (str[i] == replace[j])
-            {
-                str[i] = hexa[j];
-                break ;
-            }
-        }
+        neg = 1;
+        nb *= -1;
+        str[0] = '-';
         ++i;
     }
-    return (free_str(str));
+    while (nb)
+    {
+        str[i] = nb % base + 48;
+        nb /= base;
+        replace(nb, str, i, neg);
+        ++i;
+    }
+    return (free_str(str, neg));
+}
+
+int     main(int ac, char **av)
+{
+    if (ac != 3)
+    {
+        printf("ERROR -> usage ./[a.out] [NUMBER] [BASE]\n");
+        return (0);
+    }
+    printf("\033[1m\033[34mNumber:\033[0m %s \033[1m\033[35m Base: \033[0m%s\
+            \n\033[1m\033[32mResult: \033[0m%s\n", av[1], av[2],
+            ft_itoa_base(atoi(av[1]), atoi(av[2])));
+    return (0);
 }
