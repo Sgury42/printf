36142efef4cb966f197885264d43f53a078b82f2
Sandra <sgury@student.42.fr>
2019-05-24 10:15:14 +0200

    update

--- main.c
+++ main.c
@@ -4,9 +4,8 @@
 int		main()
 {
 	ft_putstr("my printf: ");
-	ft_printf("La vie %c est %s belle%-09d\n", 'A', "super", -100);
+	ft_printf("%+ d\n", 42);
 //	ft_putstr("rl printf: ");
-//	printf("La vie %c est %s belle%+09d\n", 'A', "super", -100);
-
+//	printf("%+ d\n", 42);
 	return (0);
 }
