3cceb29f5650772f9754873bf89955b009c4bfb8
Sandra <sgury@student.42.fr>
2019-06-03 05:42:52 +0200

    return on right output

--- ft_printf.c
+++ ft_printf.c
@@ -6,14 +6,14 @@
 /*   By: sgury <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/13 19:16:08 by sgury             #+#    #+#             */
-/*   Updated: 2019/06/03 03:09:23 by sgury            ###   ########.fr       */
+/*   Updated: 2019/06/03 05:41:27 by sgury            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include "ft_printf.h"
 
-void	ft_printf(const char *format, ...)
+int		ft_printf(const char *format, ...)
 {
 	t_data_tab	*data;
 	t_buff		buff;
@@ -22,27 +22,26 @@ void	ft_printf(const char *format, ...)
 
 	va_start(ap, format);
 	if ((data = (t_data_tab *)malloc(sizeof(t_data_tab))) == NULL)
-		exit(-1);
+		return (-1);
 	ft_memset(data, '\0', sizeof(t_data_tab));
 	ft_bzero(&buff, sizeof(t_buff));
 	buff.len = 0;
+	buff.ret = 0;
 	index = 0;
 	while ((index = ft_parse(format, data, index, &buff)) > 0)
 	{
 		if (data->conv)
 		{
 			if (ft_dispatcher(ap, data, &buff) < 0)
-				exit(-1);
+				return (-1);
 		}
 		ft_memset(data, '\0', sizeof(t_data_tab));
 	}
 	if (index < 0)
-	{
-		printf("test return");
-		exit(-1);
-	}
+		return (-1);
 	ft_putstr(buff.buffer);
+	buff.ret += buff.len;
 	va_end(ap);
 	free(data);
-	exit(buff.len);
+	return (buff.ret);
 }
--- main_test.c
+++ main_test.c
@@ -3,7 +3,7 @@
 
 int		main()
 {
-	ft_printf("%5%");
+	ft_printf("%u\n", 0);
 //	ft_printf("");
 //	ft_printf("Je fais %d characters, %s\n", 22, "en fait plutot 41");
 }
