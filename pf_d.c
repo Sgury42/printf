25c77dbc1ae7e7d86c92aea0d75a08a2cc378539
Florian <flbeaumo@student.42.fr>
2019-06-08 22:11:17 +0200

    fix %d

--- pf_d.c
+++ pf_d.c
@@ -6,20 +6,37 @@
 /*   By: flbeaumo <flbeaumo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/15 11:28:16 by flbeaumo          #+#    #+#             */
-/*   Updated: 2019/06/05 23:22:15 by sgury            ###   ########.fr       */
+/*   Updated: 2019/06/08 21:44:51 by flbeaumo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static char		*min_ll_int(unsigned long long nbr)
+{
+	char	*s1;
+	char	*s2;
+
+	if ((s1 = ft_utoa(nbr)) == NULL)
+		return (NULL);
+	s2 = ft_strjoin("-", s1);
+	ft_strdel(&s1);
+	return (s2);
+}
+
 int			pf_d(va_list ap, t_data_tab *data, t_buff *buff)
 {
 	long long int	nbr;
-	char			*str;
+	char		*str;
 
 	nbr = ft_get_nbr(ap, data);
-	if ((str = ft_itoa(nbr)) == NULL)
-		return (-1);
+	if ((unsigned long long)nbr - 1 == 9223372036854775807)
+		str = min_ll_int(nbr);
+	else
+	{
+		if ((str = ft_itoa(nbr)) == NULL)
+			return (-1);
+	}
 	if (nbr == 0 && data->flags[precision] == 'z')
 		str[0] = ' ';
 	if (data->flags[width] <= (int)ft_strlen(str))
